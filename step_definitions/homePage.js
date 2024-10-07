e78e814cea37fb98a4e39389c7bb387785153350
ronald.montano@crediminuto.com-at-070601114562 <ronald.montano@crediminuto.com>
2024-10-07 13:52:48 -0300

    Primer test cucumber

--- step_definitions/homePage.js
+++ step_definitions/homePage.js
@@ -11,18 +11,17 @@ When('the page loads', async ({ }) => {
 });
 
 
-Then('the title should be {string}', async ({ page }, inputName) => {
-console.log(inputName);
-    const inputVisible = await page.locator(`input[id='${inputName}']`).isVisible();
-    
+Then('the title should be {string}', async ({ page }, title) => {
+    await page.getByText(title).isVisible()
 });
 
 
-Then('the input {string} should be present', async ({ }, arg) => {
-    
+Then('the input {string} should be present', async ({ page }, inputName, inputPrice ) => {
+    await page.locator(`input[id='${inputName}']`).isVisible();
+    await page.locator(`input[id='${inputPrice}']`).isVisible();
 });
 
 
-Then('the button {string} should be present', async ({ }, arg) => {
-    // ...
+Then('the button {string} should be present', async ({ page }, button) => {
+    const inputVisible = await page.locator(`button[id='${button}']`).isVisible();
 });
